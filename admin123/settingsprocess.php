a288c3be24ab543c97dc284e698dc08641906a96
aravindmjgit <aravindnair93@gmail.com>
2016-04-07 13:53:35 +0530

    Super admin change password issue

--- admin123/settingsprocess.php
+++ admin123/settingsprocess.php
@@ -2,41 +2,37 @@
 require_once("db.php");
 session_start();
 $urlin="settings.php";
-$current = $_POST['txtpass']; 
-//$oldpass=$_POST['txtpass'];
-$newpass = $_POST['txtpass1'];
+$current = trim($_POST['txtpass']);
+$newpass = trim($_POST['txtpass1']);
 $encnewpass = MD5($newpass);
-$confirmpass = $_POST['txtpass2'];
+$confirmpass = trim($_POST['txtpass2']);
 
-$q = "SELECT password as pass FROM users where role_id='1'";
-
-$result=Db::query($q);
-$row=mysql_fetch_array($result);
-
-if($row['pass']==MD5($current))
-{
-       $query="UPDATE  users SET password = '$encnewpass' WHERE role_id = '1'";
-       Db::query($query);
-       json_encode(array("result"=>true));
-       $_SESSION[invalid]=1;			
+if(strlen($current)==''){
+    $_SESSION['invalid']=5;
 }
-else 
-	{
-		
-		$_SESSION[invalid]=2;
-	}
-//	header("location:settings.php");
-        if($query)
-	{
-	$_SESSION['addsucc']=1;
-		
-	}
-	else 
-	{
-	$_SESSION['addsucc']=2;
-		
-	}
-        
+else if(strlen($newpass)<5){
+    $_SESSION['invalid']=3;
+}
+else if ($newpass != $confirmpass){
+    $_SESSION['invalid']=4;
+}
+else{
+
+    $q = "SELECT password as pass FROM users where role_id='1'";
+    $result=Db::query($q);
+    $row=mysql_fetch_array($result);
+
+    if($row['pass']==MD5($current))
+    {
+       $query=sprintf("UPDATE  users SET password = '%s' WHERE role_id = '1'",$encnewpass);
+       Db::query($query);
+       $_SESSION['invalid']=1;			
+    }
+    else 
+    {
+        $_SESSION['invalid']=2;                
+    }
+}        
 echo "<script type='text/javascript'>
 location.href = '" . $urlin . "';
 </script>";?>
